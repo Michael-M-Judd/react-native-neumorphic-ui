c0e434c53e493ecfed9461f54f3bcf240e8675c6
Michael Judd <m.judd@queensu.ca>
2020-04-22 21:32:05 -0400

    fix prettier and eslint config

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -8,5 +8,6 @@ import { linkTo } from '@storybook/addon-links';
 import Welcome from './Welcome';
 import '../../src/Button/Button.story';
 import '../../src/Card/Card.story';
+import '../../src/Box/Box.story';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
