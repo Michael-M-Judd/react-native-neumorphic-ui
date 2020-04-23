a31f46fc49e3d2949300e3df95714223008a1587
Michael Judd <m.judd@queensu.ca>
2020-04-23 08:24:59 -0400

    feat: add web-build folder for gh pages

---
+++ web-build/precache-manifest.5158d9f5a20235842c1fbc22642905a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "328ec0abaf4b8f214d7f",
+    "url": "/static/js/runtime~app.b3244af7.js"
+  },
+  {
+    "revision": "4ee08ab824ae06613e18",
+    "url": "/static/js/app.aa1bf6e5.chunk.js"
+  },
+  {
+    "revision": "c0b47c8007380692d9b4",
+    "url": "/static/js/2.2412a8e4.chunk.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "04cf1ae10e30155ca207400239ec1cbd",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "027b362d7632ec59ccd47ec41b7ca6f7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "0e7c57a400d72f2b8f1fd5de5ea6c232",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  }
+];
